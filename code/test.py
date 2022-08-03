f8cc66c75e86b1315c06920dbb25d127d40af604
pial08 <pialcsedu19@gmail.com>
2022-08-02 21:02:40 -0500

    First commit from lab pc

---
+++ code/test.py
@@ -0,0 +1,10 @@
+
+
+
+import pickle
+
+
+
+filename = '../dataset/itesm-2020-10-carlos-vargas_log.pkl/itesm-2020-10-carlos-vargas_log.pkl'
+with open(filename, 'rb') as f:
+    data = pickle.load(f)
